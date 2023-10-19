2ac47a1d4cce38c8a69b0029696f0d8cc4fa1a0b
Shannqa <shannqa@gmail.com>
2023-10-19 20:16:49 +0200

    Add a button fetching new gifs and some styling

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import config from "../config";
+import "./style.css";
 
 const body = document.querySelector("body");
+const div = document.createElement("div");
 const img = document.createElement("img");
+const btnNew = document.createElement("button");
+btnNew.textContent = "More cats";
+div.classList.add("main");
+div.appendChild(btnNew);
+div.appendChild(img);
+body.appendChild(div);
+
 // giphy api key
 const apiKey = config.API_KEY;
 
-fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=cats`, {
-  mode: "cors",
-})
-  .then((response) => response.json())
-  .then((response) => {
-    img.src = response.data.images.original.url;
+function fetchGifs() {
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=cats`, {
+    mode: "cors",
   })
-  .catch((err) => {
-    console.log(err);
-  });
+    .then((response) => response.json())
+    .then((response) => {
+      img.src = response.data.images.downsized.url;
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
-body.appendChild(img);
+btnNew.addEventListener("click", () => fetchGifs());
+
+fetchGifs();
