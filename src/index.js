2905bf0f96d14e3b9bee14bb5b32687d345b5226
Shannqa <shannqa@gmail.com>
2023-10-19 20:53:19 +0200

    Handle errors

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./style.css";
 const body = document.querySelector("body");
 const div = document.createElement("div");
 const img = document.createElement("img");
+const divError = document.createElement("div");
 const btnNew = document.createElement("button");
 btnNew.textContent = "More cats";
+
 div.classList.add("main");
 div.appendChild(btnNew);
 div.appendChild(img);
+div.appendChild(divError);
 body.appendChild(div);
 
 // giphy api key
@@ -18,12 +21,21 @@ function fetchGifs() {
   fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=cats`, {
     mode: "cors",
   })
-    .then((response) => response.json())
     .then((response) => {
-      img.src = response.data.images.downsized.url;
+      if (response.status !== 200) {
+        throw new Error(`Something went wrong! Status: ${response.status}`);
+      }
       console.log(response);
+      return response.json();
+    })
+    .then((response) => {
+      divError.textContent = "";
+      // console.log(response);
+      img.src = response.data.images.downsized.url;
     })
     .catch((err) => {
+      divError.textContent =
+        "Something went wrong! Sorry, no kitties this time!";
       console.log(err);
     });
 }
