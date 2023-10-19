a92fe083d9a77cc87e55d590dbbf10b5f2f055d7
Shannqa <shannqa@gmail.com>
2023-10-19 21:06:39 +0200

    Add a reference to giphy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import config from "../config";
 import "./style.css";
+import giphy from "./Poweredby_100px-White_VertText.png";
 
 const body = document.querySelector("body");
 const div = document.createElement("div");
-const img = document.createElement("img");
+const gif = document.createElement("img");
+const powered = document.createElement("img");
+powered.src = giphy;
+const a = document.createElement("a");
+a.href = "https://giphy.com/";
 const divError = document.createElement("div");
 const btnNew = document.createElement("button");
 btnNew.textContent = "More cats";
 
 div.classList.add("main");
 div.appendChild(btnNew);
-div.appendChild(img);
+div.appendChild(gif);
 div.appendChild(divError);
+a.appendChild(powered);
+div.appendChild(a);
 body.appendChild(div);
 
 // giphy api key
@@ -25,13 +32,13 @@ function fetchGifs() {
       if (response.status !== 200) {
         throw new Error(`Something went wrong! Status: ${response.status}`);
       }
-      console.log(response);
+      // console.log(response);
       return response.json();
     })
     .then((response) => {
       divError.textContent = "";
       // console.log(response);
-      img.src = response.data.images.downsized.url;
+      gif.src = response.data.images.downsized.url;
     })
     .catch((err) => {
       divError.textContent =
